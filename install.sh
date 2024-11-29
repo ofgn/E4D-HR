a60410336ed03e9ea186c9c99029be0d1ea061e4
ofgn <69641740+ofgn@users.noreply.github.com>
2024-11-30 07:38:54 +0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -25,24 +25,24 @@ CXXOPTFLAGS="-O2 -march=native -mtune=native"
 # Defining directories
 E4D_DIR=$(pwd)
 VENDOR_DIR="${E4D_DIR}/vendor"
-PETSC_DIR="${VENDOR_DIR}/petsc-3.19.5"
+PETSC_DIR="${VENDOR_DIR}/petsc-3.21.4"
 TRI_DIR="${VENDOR_DIR}/triangle-1.6"
 TET_DIR="${VENDOR_DIR}/tetgen-1.6.0"
 
-# Create required directories if they do not exist
+# # Create required directories if they do not exist
 mkdir -p "${E4D_DIR}/bin"
 mkdir -p "$VENDOR_DIR"
 
-# Remove directories in vendor to get rid of previous configurations
+# # Remove directories in vendor to get rid of previous configurations
 rm -rf "${PETSC_DIR}" "${TRI_DIR}" "${TET_DIR}"
 
-# Unzip vendor packages
+# # Unzip vendor packages
 cd "$VENDOR_DIR" || exit 1
-tar -xzf petsc-3.19.5.tar.gz
+tar -xzf petsc-3.21.4.tar.gz
 tar -xzf triangle-1.6.tar.gz
 tar -xzf tetgen-1.6.0.tar.gz
 
-# Configure petsc
+# # Configure petsc
 cd "${PETSC_DIR}" || exit 1
 ./configure  --with-debugging=0 --with-fc="${MPIFC}" --with-cc="${MPICC}" --with-cxx="${MPICXX}" --with-blaslapack-dir="${MKLROOT}" FOPTFLAGS="${FOPTFLAGS}" COPTFLAGS="${COPTFLAGS}" CXXOPTFLAGS="${CXXOPTFLAGS}"
 make PETSC_DIR="${PETSC_DIR}" PETSC_ARCH=arch-linux-c-opt all
@@ -55,13 +55,13 @@ mv e4d "${E4D_DIR}/bin"
 find . -name '*.mod' -delete
 find . -name '*.o' -delete
 
-# Compile triangle
+# # Compile triangle
 cd "${TRI_DIR}" || exit 1
 make CC="${CC}"
 mv triangle "${E4D_DIR}/bin"
 find . -name '*.o' -delete
 
-# Compile tetgen
+# # Compile tetgen
 cd "${TET_DIR}" || exit 1
 make CXX="${CXX}"
 mv tetgen "${E4D_DIR}/bin"
